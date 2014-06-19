9501c957edc957694d3367f99a44ff0874883d61
Imanol Fernandez <imanolf@ideateca.com>
2014-06-19 22:20:20 +0200

    Regex helper class for using NSRegularExpression. Swift doesn't support native regex...

---
+++ swift2js/regex.swift
@@ -0,0 +1,38 @@
+//
+//  regex.swift
+//  swift2js
+//
+//  Created by Imanol Fernandez @MortimerGoro on 14/06/14.
+//  Copyright (c) 2014 swiftjs. All rights reserved.
+//
+
+import Foundation
+
+class Regex {
+    let pattern: String
+    let regex: NSRegularExpression
+    
+    
+    init(_ pattern:String) {
+        self.pattern = pattern
+        regex = NSRegularExpression(pattern: pattern, options: nil, error: nil)
+    }
+    
+    func test(data:String) -> Bool {
+        var match = regex.firstMatchInString(data, options: nil, range: NSMakeRange(0, data.utf16count));
+        return match != nil;
+    }
+    
+    func firstMatch(data:String) -> String? {
+        
+        var range = regex.rangeOfFirstMatchInString(data, options: nil, range: NSMakeRange(0, data.utf16count));
+        if range.location != NSNotFound {
+            return data.substringFromIndex(range.location).substringToIndex(range.length);
+        }
+        else {
+            return nil;
+        }
+    }
+    
+    
+}
