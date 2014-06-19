2d1d25528973f44f8f48a068b07d4e9c41e57cf5
Imanol Fernandez <imanolf@ideateca.com>
2014-06-19 22:28:59 +0200

    created a bridging header to allow calling C++ from Swift (Bison parser is C/C++ code)

---
+++ swift2js/bridge.h
@@ -0,0 +1,10 @@
+//
+//  cliinput-Bridging-Header.h
+//  swift2js
+//
+//  Created by Imanol Fernandez Gorostizaga on 16/06/14.
+//  Copyright (c) 2014 swiftjs. All rights reserved.
+//
+
+@class Lexer;
+void bridge_yyparse(Lexer * lexer);
